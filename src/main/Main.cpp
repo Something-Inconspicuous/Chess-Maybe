5678f0973c7670b901a3a54fce0cb44115cdf122
Something-Inconspicuous <webhead220@gmail.com>
2023-01-28 20:55:23 -0500

    addded janky clearscreen function

--- src/main/Main.cpp
+++ src/main/Main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 #include "..\..\src\libs\Board.hpp"
+#include "string"
 
 using namespace std;
 
+void clrscr(){
+    cout << string(50, '\n') << endl;
+}
+
 int main(){
     cout << "testity test" << endl;
+
+    clrscr();
 
     Board* board = new Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR");
 
